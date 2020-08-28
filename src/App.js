db91d6e763e016fae92bbf370b5115672e56045f
Thomas Nelson <thnlsn@gmail.com>
2020-08-27 22:14:25 -0700

    GoogleLogin props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
 export class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <GoogleLogin
+          clientId=''
+          buttonText=''
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
+    );
   }
 }
 export default App;
